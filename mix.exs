9234f0eafc67938b69a8445c66577f9f8eb3e6fb
Gal Tsubery <gal.tsubery@gmail.com>
2016-12-16 10:25:28 -0600

    Add metadata for hex package manager

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule GoogleFit.Mixfile do
     [app: :google_fit,
      version: "0.1.0",
      elixir: "~> 1.4.0-rc.0",
+     description: "GoogleFit API wrapper",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
   end
 
   # Configuration for the OTP application
@@ -31,7 +33,18 @@ defmodule GoogleFit.Mixfile do
       {:oauth2, "~> 0.8"},
       {:poison, "~> 3.0"},
       {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
-      {:credo, "~> 0.5", only: [:dev, :test]}
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
+  end
+
+  defp package do
+    [
+     name: :google_fit,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Gal Tsubery"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/tsubery/google_fit"}
+   ]
   end
 end
